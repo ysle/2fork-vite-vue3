acb5665620d0c4d2a85d968d2383e974827a933a
ysle <git@ysle.com>
2021-09-19 22:18:08 +0200

    init

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const { createServer } = require('vite')
+
+const run = async () => {
+	const { middlewares } = await createServer({ server: { middlewareMode: 'html' } })
+
+	express() //
+		.get('/api', (req, res) => res.json(Math.random()))
+		.use(middlewares)
+		.listen(8080)
+}
+
+run()
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+	<pre>{{ msg }}</pre>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('loading ...')
+
+setInterval(async () => {
+	msg.value = await fetch(`${location.origin}/api`).then(r => r.json())
+}, 500)
+</script>
