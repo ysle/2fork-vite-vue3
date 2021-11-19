44b6e544a1894786824971b7ccefb8574cf542aa
ysle <git@ysle.com>
2021-11-19 21:06:05 +0100

    dep update

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
 const msg = ref('loading ...')
 
-setInterval(async () => {
-	msg.value = await fetch(`${location.origin}/api`).then(r => r.json())
-}, 500)
+onMounted(() => {
+	setInterval(async () => {
+		msg.value = await fetch(`${location.origin}/api`).then(r => r.json())
+	}, 500)
+})
 </script>
